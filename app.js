00f15ff031945fd7359ceebc269de6f9f15ae9ae
Grant Whiteman <grantwhiteman95@gmail.com>
2021-08-03 17:19:30 +0100

    added basic form and post request!!!!!    !!!!!!!

--- app.js
+++ app.js
@@ -4,12 +4,23 @@ const port = 3000
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended:true }))
+
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.render('index.ejs', {
+        name: req.body.name,
+        url: req.body.url,
+        comment: req.body.comment
+    })
 })
+
+app.post('/', (req, res) => {
+    res.render('index.ejs', {
+        name: req.body.name,
+        url: req.body.url,
+        comment: req.body.comment
+    })
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
-
-
-
--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,9 +1,15 @@
-
-
-
 describe('load page',() => {
     it('displays bookmark list', () =>{
         cy.visit('/')
         cy.get('h1').should('contain', 'Bookmark Manager!')
+    })
+
+    it('displays bookmark list', () =>{
+        cy.visit('/')
+        cy.get('#name_input').type('Aldi')
+        cy.get('#url_input').type('aldi.com')
+        cy.get('#comment_input').type("Lottie's favourite store")
+        cy.get('#submit').click()
+        cy.get('#bookmarks').should('contain', 'Aldi')
     })
 })
