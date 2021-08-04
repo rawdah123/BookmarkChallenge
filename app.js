0735836947221ba114484593646f5bf565aaffdf
Grant Whiteman <grantwhiteman95@gmail.com>
2021-08-04 17:33:32 +0100

    added delete functionality

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000;
 require('dotenv').config();
 const { models, sequelize } = require('./models');
+const methodOverride = require('method-override');
 
+app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
@@ -15,15 +17,25 @@ app.get('/', async (req, res) => {
 });
 
 app.post('/', async (req, res) => {
-    sequelize.sync().then(async () => {
-        await models.Bookmark.create({ 
-            name: req.body.name,
-            url: req.body.url,
-            comment: req.body.comment
-        })
-    })
-    res.redirect('/')
-})
+	sequelize.sync().then(async () => {
+		await models.Bookmark.create({
+			name    : req.body.name,
+			url     : req.body.url,
+			comment : req.body.comment
+		});
+	});
+	res.redirect('/');
+});
+
+app.delete('/bookmark/:id', async (req, res) => {
+	console.log(req.params);
+	await models.Bookmark.destroy({
+		where : {
+			id : req.params.id
+		}
+	});
+	res.redirect('/');
+});
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
