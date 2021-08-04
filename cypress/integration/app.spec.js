519a75ddc3e6fbe0a9b0de928fd4522de000ba14
unknown <char.jon3s@gmail.com>
2021-08-04 15:31:47 +0100

    database added

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -13,3 +13,18 @@ describe('load page',() => {
         cy.get('#bookmarks').should('contain', 'Aldi')
     })
 })
+describe('bookmarks',() => {
+    it('can add bookmark', () =>{
+        cy.visit('/')
+     
+    })
+
+    it('displays bookmark list', () =>{
+        cy.visit('/')
+        cy.get('#name_input').type('Aldi')
+        cy.get('#url_input').type('aldi.com')
+        cy.get('#comment_input').type("Lottie's favourite store")
+        cy.get('#submit').click()
+        cy.get('#bookmarks').should('contain', 'Aldi')
+    })
+})
